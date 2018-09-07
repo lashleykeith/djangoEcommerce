70efdd4d86d4f99a6b421d8663b839e3b17bc494
Keith Lashley <lashleykeith@gmail.com>
2018-09-08 01:20:32 +0900

    Add files via upload

---
+++ myshop/shop/migrations/0005_migrate_translatable_fields.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0004_remove_untranslated_fields'),
+    ]
+
+    operations = [
+    ]
---
+++ myshop/shop/migrations/0006_migrate_translatable_fields.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0005_migrate_translatable_fields'),
+    ]
+
+    operations = [
+    ]
